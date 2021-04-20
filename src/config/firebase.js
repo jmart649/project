c6624ef954ff0cd4cc515696c3b102b7da6901c5
jmart649 <martinez.jesse84@yahoo.com>
2021-04-19 20:28:38 -0500

    change

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC874b6nzdZ5ByPqfuGgZlclBC3yxqO6Lw",
+    authDomain: "jmartloginexample.firebaseapp.com",
+    projectId: "jmartloginexample",
+    storageBucket: "jmartloginexample.appspot.com",
+    messagingSenderId: "21668467997",
+    appId: "1:21668467997:web:46d86f50079aa83772fdf4"
+  };
